2f3196b9910d38dffcf677ab1b3a7d318fb0a852
Connor <cmail9@yahoo.com>
2026-03-24 08:12:49 -0400

    Clear WSDL cache and rebuild interface document when URL changes

--- app/soap/service.py
+++ app/soap/service.py
@@ -368,7 +368,11 @@ class _ProxyAwareWsgiApplication(WsgiApplication):
         logger.debug("WSDL request: url=%s, cached_url=%s", url, self._wsdl_url)
         if self._wsdl_url != url:
             logger.info("WSDL address changed: %s -> %s", self._wsdl_url, url)
+            # Clear both the wsgi-level cache and the wsdl11 internal cache
+            # so build_interface_document is called with the new URL
             self._wsdl = None
+            self.doc.wsdl11.build_interface_document(url)
+            self._wsdl = self.doc.wsdl11.get_interface_document()
             self._wsdl_url = url
         return super().handle_wsdl_request(req_env, start_response, url)
 
